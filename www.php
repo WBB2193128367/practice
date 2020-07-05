c9d743ea29a900615a513d26be6146dfe4831c9e
WBB2193128367 <wang09181821@icloud.com>
2020-07-05 19:14:26 +0800

    增加属性

--- www.php
+++ www.php
@@ -1,9 +1,11 @@
 <?php
 class Student{
+    public $number;
     public $name;
     public $age;
-    public function __construct($age,$name)
+    public function __construct($age,$name,$number)
     {$this->age=$age;
     $this->name=$name;
+    $this->number=$number;
     }
 }
